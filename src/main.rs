5a655171dc82b2cc337550a093707f95b9c3ee69
Peter Allin <peter@peca.dk>
2018-09-02 01:22:20 +0200

    Make a list of branches and print that instead of printing directly

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,26 @@ extern crate git2;
 
 use git2::{Repository, BranchType};
 
-fn run() -> Result<(), git2::Error> {
-    let repo = Repository::open("/home/pal/rust_again/renamee")?;
+fn find_branches(repo_path: &str) -> Result<Vec<String>, git2::Error> {
+    let mut result = Vec::new();
+    let repo = Repository::open(repo_path)?;
     let branches = repo.branches(Some(BranchType::Local))?;
     for maybe_branch in branches {
         let branch = maybe_branch?;
-        println!("Branch: {:?}", branch.0.into_reference().name())
+        match branch.0.into_reference().name() {
+            Some(b) => result.push(b.to_string()),
+            None => {}
+        }
     };
     repo.set_head("refs/heads/old")?;
+    Ok(result)
+}
+
+fn run() -> Result<(), git2::Error> {
+    let branches = find_branches("/home/pal/rust_again/renamee")?;
+    for b in branches {
+        println!("Branch: {}", b);
+    }
     Ok(())
 }
 
