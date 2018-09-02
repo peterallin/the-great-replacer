aa6e71e7388228a288f779b50e934b935b77353a
Peter Allin <peter@peca.dk>
2018-09-03 00:16:29 +0200

    Use command line arguments in place of hardcoded values

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+extern crate clap;
+
 mod gittools;
 use gittools::{commit_single_file, do_in_branches, find_branches, Repository};
 
 mod replace;
 
 use std::path::Path;
+use clap::{App, Arg};
 
 fn replace(
     repo_path: &Path,
@@ -31,14 +34,56 @@ fn replace(
 }
 
 fn main() {
+    let matches = App::new("the-great-replacer")
+        .version("0.1.0")
+        .about("Replaces string in all git branches")
+        .author("Peter Allin")
+        .arg(Arg::with_name("repopath")
+            .long("repopath")
+            .value_name("PATH")
+            .required(true))
+        .arg(Arg::with_name("filename")
+            .long("filename")
+            .value_name("FILENAME")
+            .required(true))
+        .arg(Arg::with_name("username")
+            .long("username")
+            .value_name("USERNAME")
+            .required(true))
+        .arg(Arg::with_name("email")
+            .long("email")
+            .value_name("EMAIL")
+            .required(true))
+        .arg(Arg::with_name("from")
+            .long("from")
+            .value_name("FROM")
+            .required(true))
+        .arg(Arg::with_name("to")
+            .long("to")
+            .value_name("TO")
+            .required(true))
+        .arg(Arg::with_name("message")
+            .long("message")
+            .value_name("MESSAGE")
+            .required(true))
+        .get_matches();
+
+    let repo_path = matches.value_of("repopath").unwrap();
+    let file_name = matches.value_of("filename").unwrap();
+    let user_name = matches.value_of("username").unwrap();
+    let email = matches.value_of("email").unwrap();
+    let from =  matches.value_of("from").unwrap();
+    let to =  matches.value_of("to").unwrap();
+    let message =  matches.value_of("message").unwrap();
+
     match replace(
-        Path::new("/home/pal/rust_again/renamee"),
-        "snafu",
-        "U",
-        "UU",
-        "The Great Replacer",
-        "repl@repl.repl",
-        "Replaced by the great replacer",
+        Path::new(repo_path),
+        file_name,
+        from,
+        to,
+        user_name,
+        email,
+        message,
     ) {
         Ok(_) => {}
         Err(e) => {
